69389c290204c04526dea35abc764bcc4e960a94
Rony Moura <ronymmoura@gmail.com>
2024-06-04 01:07:17 -0300

    fix: account sql tests

--- db/sqlc/account.sql_test.go
+++ db/sqlc/account.sql_test.go
@@ -50,10 +50,8 @@ func TestGetAccount(t *testing.T) {
 }
 
 func TestListAccounts(t *testing.T) {
-	var lastAccount Account
-
 	for i := 0; i < 10; i++ {
-		lastAccount = createRandomAccount(t)
+		createRandomAccount(t)
 	}
 
 	arg := ListAccountsParams{
